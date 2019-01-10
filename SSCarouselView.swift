9e1e6ccd75d1d16fd3be3cfbc14ad3e30208222d
Kyle <yangzychina@gmail.com>
2019-01-10 14:29:57 +0800

    轮播图

---
+++ SSCarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  SSCarouselView.swift
+//  FBSnapshotTestCase
+//
+//  Created by Kyle on 2019/1/10.
+//
+
+import UIKit
+
+class SSCarouselView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
